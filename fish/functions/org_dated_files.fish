2e482df97c4c2bb60b7bb30b6f8567bd04e9309b
Nachikethas AJ <nachikethas@users.noreply.github.com>
2024-11-04 15:20:44 -0800

    [fish] Add function to organize dated files
    A "dated file" is a file whose file name includes a date relevant to the
    file contents. Currently, this function only supports dates in ISO 8601
    format.

---
+++ fish/functions/org_dated_files.fish
@@ -0,0 +1,23 @@
+function org_dated_files
+  argparse 'd/description=' 's/suffix=' 'p/path=!test -d "$_flag_value"' -- $argv
+  or return
+  for file in $argv
+    if not test -f $file
+      echo 'Given argument is not a file: $file'
+      return 1
+    end
+    set -l re_year '(?<year>\d{4})'
+    set -l re_month '(?<month>0[1-9]|1[0-2])'
+    set -l re_day '(?<day>0[1-9]|[12][0-9]|3[01])'
+    string match -rqg (string join '' '(?<file_date>' (string join '\-' $re_year $re_month $re_day) ')') $file
+    if test -z $file_date
+      echo 'Could not extract date from file name (ISO 8601 yyyy-mm-dd): ' $file_date
+      return 1
+    end
+    set -l prefix (string replace -a - '' $file_date)
+    set -l extension (string lower (path extension $file))
+    set dst_file (path normalize (string join '/' $_flag_path (path change-extension $extension (string join _ $prefix $_flag_description $_flag_suffix))))
+    cp $file $dst_file
+    echo "Copied $file to $dst_file"
+  end
+end
