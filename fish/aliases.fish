e6722c41d29af94df907068007fbdee38b5ef4d7
Nachikethas AJ <nachikethas@users.noreply.github.com>
2026-03-29 15:57:37 -0700

    [fish] Hyperlink and don't deference symbolic links
    Also removed obsolete aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -2,10 +2,10 @@ alias cat 'bat'
 alias ping 'prettyping --nolegend'
 alias top 'htop'
 alias du 'ncdu --color dark -rr -x'
-alias l 'eza --group-directories-first --classify --icons --dereference'
-alias ls 'eza --icons'
-alias la 'eza --group-directories-first --classify --icons --dereference -a'
-alias ll 'eza --group-directories-first --classify --icons --dereference -l'
+alias ls 'eza --group-directories-first --classify --icons --hyperlink'
+alias l 'ls'
+alias la 'eza --group-directories-first --classify --icons -a --hyperlink'
+alias ll 'eza --group-directories-first --classify --icons -l --hyperlink'
 alias rm 'rm -I'
 alias bkup 'btrbk -c .config/btrbk.conf -v --pretty --progress run'
 
@@ -14,10 +14,3 @@ alias .. 'cd ..'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
-
-# Enable native Wayland backend in Signal
-alias signal-desktop 'signal-desktop --ozone-platform=wayland --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-features=WaylandWindowDecorations'
-
-# Kill tracker-miner-fs deamon. See
-# https://gnome.pages.gitlab.gnome.org/tracker/faq/#how-can-i-help-debug-problems-with-tracker-services
-alias kill_tracker 'systemctl mask --user --now tracker-miner-fs-3.service'
