6dd95960b00c842cede0fbfe1f417fc8d4f65fdd
Nachikethas AJ <nachikethas@users.noreply.github.com>
2026-04-19 10:00:45 -0700

    [nvim] Add render-markdown

--- nvim/plugin/ui.lua
+++ nvim/plugin/ui.lua
@@ -1,5 +1,6 @@
 vim.pack.add({
   { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
 })
 
 require('catppuccin').setup({ auto_integrations = true })
